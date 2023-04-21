030b680a7d653aca73b8859c70f6e46ff9e26af1
W41K3R0 <mahapatrarik123@gmail.com>
2023-04-21 01:10:43 +0000

    First commit after adding and checking the api

--- helpers/app.js
+++ helpers/app.js
@@ -1,1 +1,21 @@
-console.log("hello")
+const TOTALNUMOFPOKEMON = 1015
+
+function fetchPoke(a)
+{
+    url = `https://pokeapi.co/api/v2/pokemon/${a}`
+
+    fetch(url)
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            console.log(data)
+        })
+}
+
+fetchPoke(1)
+
+// for(let i=1;i<=TOTALNUMOFPOKEMON;i++)
+// {
+//     fetchPoke(i)
+// }
